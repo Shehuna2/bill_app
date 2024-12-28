14d76c8e47011f622a441aebfb9e57fd1cbe44a7
Shehuna2 <135225592+Shehuna2@users.noreply.github.com>
2024-12-28 10:45:12 +0100

    update urls.py

--- services/urls.py
+++ services/urls.py
@@ -3,4 +3,6 @@ from . import views
 
 urlpatterns = [
     path('purchase/<int:service_id>/', views.purchase_service, name='purchase_service'),
+    path('history/', views.purchase_history, name='purchase_history'),
 ]
+
