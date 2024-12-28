c1745941bdaebcd730ee5776d598a68776cb4d7c
Shehuna2 <135225592+Shehuna2@users.noreply.github.com>
2024-12-28 13:17:48 +0100

    update urls.py

--- wallet/urls.py
+++ wallet/urls.py
@@ -4,5 +4,5 @@ from . import views
 urlpatterns = [
     path("deposit/", views.deposit_view, name="wallet_deposit"),
     path("deposit/verify/", views.verify_deposit_view, name="wallet_verify"),
-    path("dashboard/", views.wallet_dashboard, name="wallet_dashboard"),
+    path("", views.wallet_dashboard, name="wallet_dashboard"),
 ]
